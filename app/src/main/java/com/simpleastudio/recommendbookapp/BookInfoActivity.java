cb86483d4d125969ebb2e29810a1009daa417cbd
JonnyLee <jonnylee719@gmail.com>
2015-10-09 12:18:45 -0300

    BookInfoActivity extends SingleFragmentActivity:

--- app/src/main/java/com/simpleastudio/recommendbookapp/BookInfoActivity.java
+++ app/src/main/java/com/simpleastudio/recommendbookapp/BookInfoActivity.java
@@ -3,18 +3,14 @@ package com.simpleastudio.recommendbookapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class BookInfoActivity extends AppCompatActivity {
-
+public class BookInfoActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    protected Fragment createFragment() {
+        return new BookInfoFragment();
     }
-
 }
