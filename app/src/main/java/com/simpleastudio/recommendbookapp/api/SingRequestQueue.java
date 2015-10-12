25ec0746be3daa454a9a74ab9441813ceb07e312
JonnyLee <jonnylee719@gmail.com>
2015-10-12 19:46:54 -0300

    Wrote singleton RequestQueue class

--- app/src/main/java/com/simpleastudio/recommendbookapp/api/SingRequestQueue.java
+++ app/src/main/java/com/simpleastudio/recommendbookapp/api/SingRequestQueue.java
@@ -1,7 +1,11 @@
 package com.simpleastudio.recommendbookapp.api;
 
+import android.content.Context;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 
 /**
  * Created by Jonathan on 12/10/2015.
@@ -10,4 +14,33 @@ public class SingRequestQueue {
     private static SingRequestQueue mInstance;
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
+    private static Context mCxt;
+
+    private SingRequestQueue(Context c){
+        mCxt = c;
+        mRequestQueue = getRequestQueue();
+        mImageLoader = new ImageLoader(mRequestQueue, new LruBitmapCache(LruBitmapCache.getCacheSize(mCxt)));
+    }
+
+    public static synchronized  SingRequestQueue getInstance(Context c){
+        if(mInstance == null){
+            mInstance = new SingRequestQueue(c);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(mRequestQueue == null){
+            mRequestQueue = Volley.newRequestQueue(mCxt.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req){
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader(){
+        return mImageLoader;
+    }
 }
